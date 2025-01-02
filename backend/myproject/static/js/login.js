2b0459d7de94c2ab9e58d4e279fe369bfd4cb7f3
Leo222004 <brebenelleonardcristian@gmail.com>
2025-01-02 18:30:29 +0200

    Am rezolvat toate problemele legate de autentificare si inregistrare

--- backend/myproject/static/js/login.js
+++ backend/myproject/static/js/login.js
@@ -1,3 +1,7 @@
 document.querySelector('.login-form').addEventListener('submit', function (event) {
   console.log('Form submitted'); // Adaugă acest mesaj pentru debugging
 });
+
+document.querySelector('.register-btn').addEventListener('click', function() {
+  window.location.href = this.getAttribute('data-url');
+});
--- backend/myproject/users/admin.py
+++ backend/myproject/users/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'phone_number', 'is_admin', 'is_staff', 'is_active', 'is_superuser']
+    list_filter = ['is_admin', 'is_staff', 'is_active', 'is_superuser']
+    search_fields = ['username', 'email', 'phone_number']
+    ordering = ['username']
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone_number', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('date_joined',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone_number', 'password1', 'password2')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+admin.site.register(CustomUser, CustomUserAdmin)
